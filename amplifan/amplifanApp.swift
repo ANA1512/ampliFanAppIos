0b3d8b77b2a8dd5752a173928a00bc337fb2b5de
Philocles anais <anais_philocles@yahoo.fr>
2025-06-26 09:06:50 +0200

    Initial Commit

---
+++ amplifan/amplifanApp.swift
@@ -0,0 +1,17 @@
+//
+//  amplifanApp.swift
+//  amplifan
+//
+//  Created by Philocles anais  on 26/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct amplifanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
