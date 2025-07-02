717208905cecd07869bf376be7cae71533039bd1
Philocles anais <anais_philocles@yahoo.fr>
2025-07-02 16:25:53 +0200

    last page final score quiz

--- amplifan/View/ScoreView.swift
+++ amplifan/View/ScoreView.swift
@@ -12,13 +12,36 @@ struct ScoreView: View {
     let restart:() -> Void
     
     var body: some View {
-        VStack {
-            Text("your score is : \(finalScore)")
-            Button("back to the quiz"){
-               restart()
+        
+            VStack {
+                
+                Image("trophy")
+                    .resizable()
+                    .scaledToFit()
+                     .frame(width: 150, height: 150)
+                    .padding(.top, 10)
+                
+                //display score
+                Text("Your score is : \(finalScore)")
+                    .foregroundColor(Color.yellow)
+                    .font(.system(size: 25, weight: .bold))
+                
+                    .padding(20)
+                
+                
+                //button
+                Button(action: {
+                    restart()
+                }) {
+                    Label("Replay", systemImage: "arrow.clockwise.circle")
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                
             }
-        }
     }
 }
 
-
+#Preview {
+    ScoreView(finalScore: 30, restart:{})
+}
